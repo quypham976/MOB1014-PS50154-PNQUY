2f2f5828e38b90c885e89e5f4a6c24735302b0fc
phamquy1304 <snbdjb976@gmail.com>
2026-09-18 23:45:05 +0700

    Hoan thanh Video lab 1

---
+++ LAB1/lab1/src/main/java/com/poly/lab1/hellojava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.lab1;
+
+/**
+ *
+ * @author Acer
+ */
+public class hellojava {
+    public static void main(String[] args) {
+        System.out.printf("Ho Ten: Pham Nguyen Huu Quy\nMssv: PS50154");
+    }
+}
+   
